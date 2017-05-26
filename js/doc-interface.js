0204e2148be59000139c0abec718a7b4034ffc9f
Grady L Shelton <gradyish@gmail.com>
2017-05-26 10:17:34 -0700

    show doctors matching a condition

--- js/doc-interface.js
+++ js/doc-interface.js
@@ -1,8 +1,16 @@
 var Doctor = require('./../js/doc.js').doctorModule;
 
-$(document).ready(function(){
+var displayDocs = function(doctors) {
+  doctors.forEach(function(doctor) {
+    $('.docResults').append(`<li>${doctor.profile.last_name}</li>`);
+  });
+};
+
+$(document).ready(function() {
   var doctor = new Doctor();
-  $("#button").submit(function(){
-    
+  $("#condition-form").submit(function(e){
+    e.preventDefault();
+    condition = $('#condition').val();
+    doctor.doctorSearch(condition, displayDocs);
   });
 });
--- js/doc.js
+++ js/doc.js
@@ -3,14 +3,18 @@ var apiKey = require('./../.env').apiKey;
 function Doctor(){
 }
 
-Doctor.prototype.doctorSearch = function(medicalIssue){
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-      console.log(result);
+Doctor.prototype.doctorSearch = function(issue, displayDocs) {
+  // alert(medicalIssue);
+  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=${apiKey}`)
+   .then(function(response) {
+      console.log(response.data[1].profile.last_name);
+      console.log(response);
+      displayDocs(response.data);
     })
    .fail(function(error){
-      console.log("fail");
+
     });
 };
+
 
 exports.doctorModule = Doctor;
