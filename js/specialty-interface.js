5c62ab6bc3f507443a2f8cf8a4e3fbbe4f5c0265
Grady L Shelton <gradyish@gmail.com>
2017-05-26 13:06:17 -0700

    add doctor search based on specialization

--- js/specialty-interface.js
+++ js/specialty-interface.js
@@ -1,14 +1,32 @@
 var Specialty = require('./../js/specialty.js').specialtyModule;
 
 var dropDown = function(specialties) {
-
   specialties.forEach(function(specialty) {
     console.log(`<option>${specialty.actor}</option>`);
-    $('#specialty').append(`<option>${specialty.actor}</option>`);
+    $('#specialty').append(`<option value='${specialty.uid}'>${specialty.actor}</option>`);
+  });
+};
+
+var specialDocs = function(doctors) {
+  doctors.forEach(function(doctor) {
+    $('.docResults').append(`<p><b>Dr. ${doctor.profile.first_name} ${doctor.profile.last_name}</b></p>
+      <p>Specialty: ${doctor.specialties[0].name}</p>
+      <p>Office: ${doctor.practices[0].name}</p>
+      <p>Location: ${doctor.practices[0].visit_address.street} ${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state}</p>
+      <p>Phone: ${doctor.practices[0].phones[0].number}</p>
+      <p>Bio: ${doctor.profile.bio}</p><br>
+      <img class="bio-pics" src="${doctor.profile.image_url}" alt="dr image"/>
+      <hr>`);
   });
 };
 
 $(document).ready(function() {
-  var specialDocs = new Specialty();
-  specialDocs.specializations(dropDown);
+  var specialDoctors = new Specialty();
+  specialDoctors.specializations(dropDown);
+  $('#specialty-form').submit(function(e){
+    e.preventDefault();
+    specialty = $('#specialty').val();
+    specialDoctors.specialSearch(specialty, specialDocs);
+    $('.results').show();
+  });
 });
--- js/specialty.js
+++ js/specialty.js
@@ -14,4 +14,14 @@ Specialty.prototype.specializations = function(dropDown){
   });
 };
 
+Specialty.prototype.specialSearch = function(specialty, specialDocs) {
+  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=${specialty}&location=37.773%2C-122.413%2C20&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`)
+  .then(function(response) {
+    specialDocs(response.data);
+  })
+  .fail(function(error){
+    $('.docResults').text("Unfortunately no doctors were found in this query");
+  });
+};
+
 exports.specialtyModule = Specialty;
